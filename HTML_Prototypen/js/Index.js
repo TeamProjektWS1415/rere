8ce51b09e62db3d69987f3d57d96708b5978c836
Felix Hohlwegler (holzfelix) <info@felix-hohlwegler.de>
2014-12-02 17:56:32 +0100

    button interaktionen

--- HTML_Prototypen/js/Index.js
+++ HTML_Prototypen/js/Index.js
@@ -3,5 +3,18 @@ $(document).ready(function () {
     // tooltips
     $('[data-toggle="tooltip"]').tooltip();
 
+    // delete Fach button reaktion
+    $('.deletefach').click(function () {
+        if (confirm("Wollen Sie das Fach mit allen zugehoerenden Noten wirklich loeschen?") === true) {
+            // Hier kommt dann der aufruf zum löschen
+        }
+    });
+
+    // delete Modul button reaktion
+    $('.modulloeschenbutton').click(function () {
+        if (confirm("Wollen Sie das Modul mit allen zugehoerenden Faechern und Noten wirklich loeschen?") === true) {
+            // Hier kommt dann der aufruf zum löschen
+        }
+    });
 
 });
