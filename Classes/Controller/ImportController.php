18b1c4477d5adf8716a59e0f18d02c5f2291f5a1
unknown <info@felix-hohlwegler.de>
2015-02-15 15:02:36 +0100

    Import fortschritt

--- Classes/Controller/ImportController.php
+++ Classes/Controller/ImportController.php
@@ -15,6 +15,11 @@ class ImportController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
     const IMPORT = "Import";
     const IMPORTKLEIN = "import";
 
+    private $passfunctions = NULL;
+    private $userfunctions = NULL;
+    private $mailfunctions = NULL;
+    private $persistenceManager = NULL;
+
     /**
      * Protected Variable helper wird mit NULL initialisiert.
      *
@@ -64,6 +69,32 @@ class ImportController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
     protected $FrontendUserGroupRepository = NULL;
 
     /**
+     * Protected Variable settingsRepository wird mit NULL initialisiert.
+     *
+     * @var \ReRe\Rere\Domain\Repository\SettingsRepository
+     * @inject
+     */
+    protected $settingsRepository = NULL;
+
+    /**
+     * Protected Variable objectManager wird mit NULL initialisiert.
+     *
+     * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
+     * @inject
+     */
+    protected $objectManager = NULL;
+
+    /**
+     * Im Konstruktor des ImportControllers werden Instanzen der Helper-Functions erzeugt.
+     */
+    public function __construct() {
+        $this->passfunctions = new \ReRe\Rere\Services\NestedDirectory\PasswordFunctions();
+        $this->userfunctions = new \ReRe\Rere\Services\NestedDirectory\UserFunctions();
+        $this->mailfunctions = new \ReRe\Rere\Services\NestedDirectory\ReReMailer();
+        $this->persistenceManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager');
+    }
+
+    /**
      * View-Rendering für Import
      */
     public function newAction() {
@@ -88,21 +119,19 @@ class ImportController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
      */
     public function importPrueflingeAction() {
         // Holt alle Usergroups
-        $usergroupss = $this->FrontendUserGroupRepository->findAll();
+        $usergroup = $this->request->getArgument("usergroup");
         if ($this->request->hasArgument(self::IMPORTKLEIN) && $_FILES[self::IMPORTKLEIN]['error'] == 0) {
+
             // Holt die Datei
             $file = $this->request->getArgument(self::IMPORTKLEIN);
             $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
-            echo "<br><br>Name " . $file['name'];
-            echo "<br><br>Extension " . $ext;
+            // Prüfung ob die Dateiendung korrekt ist.
             if ($ext != "csv") {
-
                 echo "Falsche Dateiendung";
                 $this->addFlashMessage('Falsche Dateiendung, es sind nur CSV-Dateien gültig.', '', \TYPO3\CMS\Core\Messaging\AbstractMessage::ERROR);
-
                 $this->view->assignMultiple(array(self::TITLE => 'Import Prüflinge', self::LABLE => 'CSV-Datei mit Prüflingen', type => $this->request->getArgument('type'), usergroups => $usergroupss));
             }
-            $this->parseCSV($file["tmp_name"]);
+            $this->parseCSV($file["tmp_name"], $usergroup);
         } else {
             $this->addFlashMessage('Keine Datei gewählt', '', \TYPO3\CMS\Core\Messaging\AbstractMessage::ERROR);
         }
@@ -121,7 +150,7 @@ class ImportController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
      * Parst eine CSV Datei.
      * @param type $file
      */
-    protected function parseCSV($file) {
+    protected function parseCSV($file, $usergroup) {
         // Zählen der Zeilen - Wird benötit um die letzte zeile zu ignorieren.
         $csvFileForLines = fopen($file, "r");
         $numberOfLines = 0;
@@ -144,14 +173,68 @@ class ImportController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
             if ($row > 5 && $numberOfLines >= $row) {
                 for ($c = 0; $c < $num; $c++) {
                     array_push($pruefling, $data[$c]);
-                    echo $data[$c] . "<br />\n";
                 }
             }
+            $this->createPruefling($pruefling, $usergroup);
+        }
+        fclose($csvFile);
+    }
 
-            var_dump($pruefling);
+    protected function createPruefling($prueflingInfos, $usergroupIN) {
+
+        echo "WAS AN KAM " . $prueflingInfos[0] . "<br><br>";
+
+        echo "Mailadresse: " . $prueflingInfos[7];
+
+        $pruefling = $this->objectManager->create('\\ReRe\\Rere\\Domain\\Model\\Pruefling');
+
+        $pruefling->setVorname($prueflingInfos[2]);
+        $pruefling->setNachname($prueflingInfos[1]);
+        $pruefling->setMatrikelnr($prueflingInfos[0]);
+
+        $allPrueflinge = $this->prueflingRepository->findAll();
+        $status = true;
+
+        foreach ($allPrueflinge as $p) {
+            if ($p->getMatrikelnr() == $pruefling->getMatrikelnr()) {
+                echo "gibts scho";
+                $status = false;
+                break;
+            }
         }
 
-        fclose($csvFile);
+        // Prueft, ob diese MatrikelNr bereits vorhanden ist. Pruefling wird nur angelegt, wenn die MatrikelNr noch nicht verwendet wird!
+        if ($status && $pruefling->getMatrikelnr() != NULL) {
+
+            $this->prueflingRepository->add($pruefling);
+
+            $usergroup = $this->FrontendUserGroupRepository->findByUid($usergroupIN);
+            // Instanz eines neuen Users
+            $newFEUser = new \Typo3\CMS\Extbase\Domain\Model\FrontendUser();
+
+            // Neuen TYPO3 FE_User anlegen
+            $newFEUser->setUsername($this->userfunctions->genuserNAME($pruefling->getVorname(), $pruefling->getNachname()));
+            // Passwort-Generierung -> Random und dann -> Salt
+            $randomPW = $this->passfunctions->genpassword();
+            $saltedPW = $this->passfunctions->hashPassword($randomPW);
+            $newFEUser->setPassword($saltedPW);
+            $newFEUser->setNAME($randomPW);
+            $newFEUser->setFirstNAME($pruefling->getVorname());
+            $newFEUser->setLastNAME($pruefling->getNachname());
+            $newFEUser->setEmail($prueflingInfos[7]);
+            $newFEUser->setPID($usergroup->getPid());
+
+            // Wenn Usergroup vorhanden dann wird diese gesetzt.
+            $newFEUser->addUsergroup($usergroup);
+
+            $absender = $this->settingsRepository->findByUid(1)->getMailAbsender();
+
+            $this->FrontendUserRepository->add($newFEUser);
+            $pruefling->setTypo3FEUser($newFEUser);
+            $this->mailfunctions->newUserMail($newFEUser->getEmail(), $newFEUser->getUsername(), $pruefling->getNachname(), $pruefling->getVorname(), $randomPW, $absender);
+        } else {
+            echo 'ERROR';
+        }
     }
 
 }
