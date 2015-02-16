58e5ebd1a6572fdf498459c1a68a75ad6b622783
ti-wa <tim@tims-MacBook-Air.local>
2015-02-16 18:39:06 +0100

    ImportAlt: Import Subjects unvollständig
    Referenz des Moduls in der neuen Datenbank wird nun gesucht.

--- ImportAltdaten/import.php
+++ ImportAltdaten/import.php
@@ -65,13 +65,57 @@ $subjects_Array=array_map('trim', $subjects_Array);
 $grades_Array=array_map('trim', $grades_Array);
 $modules_Array=array_map('trim', $modules_Array);
 
-
-// ** Import Modules ** 
+/*
+// ** Import Modules ** /
 for ($i = 0; $i < count($modules_Array); $i=$i+8){
 	$sql = "INSERT INTO tx_rere_domain_model_modul (pid, modulnr, modulname, gueltigkeitszeitraum, fach, tstamp, crdate, cruser_id, deleted, hidden) VALUES (".$PID.",".$MODULNUMMER.",".$modules_Array[$i+7].",'".$GUELTIGKEITSZEITRAUM."',".$FACH.",".$modules_Array[$i+2]. ",".$modules_Array[$i+3].",".$modules_Array[$i+4].",".$modules_Array[$i+5].",".$modules_Array[$i+6]." )";		
 	if (!mysqli_query($db, $sql)) {
 		echo "<br>Error: " . $sql . "<br>" . mysqli_error($db);   
 	}	
 }
+*/
+// ** Import Subjects **
+for ($i = 0; $i < count($subjects_Array); $i=$i+9){
+	$subjectUID = $subjects_Array[$i];
+	
+	//search for related entry in addministration table 	
+	$subjectUID_FK = -1;
+	$linePointer = 0;
+	for ( ;$linePointer < count($administrations_Array); $linePointer=$linePointer+15){
+		$subjectUID_FK = $administrations_Array[$linePointer+8];
+		if($subjectUID_FK == $subjectUID){
+			break;
+		}
+	}
+	
+	if($subjectUID_FK == -1){
+		echo "Error: Subject without ForeignKey in administration table";
+		exit();	
+	}
+	//search for ModulUID in new db for reference 
+	$modulUIDforSubject_old = $administrations_Array[$linePointer+7];
+	$modulNameforSubject = -1;
+	for($k = 0; $k < count($modules_Array); $k=$k+8){
+		if($modules_Array[$k]== $modulUIDforSubject_old){
+			$modulNameforSubject = $modules_Array[$k+7];
+		}
+	}	
+	if($modulNameforSubject == -1){
+		echo "Error: no modul with UID: " . $modulUIDforSubject_old . "in CSV";
+	}
+	$modulNameforSubject = "sote";
+	$sqlQuery = "SELECT uid FROM tx_rere_domain_model_modul where modulname='".$modulNameforSubject."'";
+	$result  = mysqli_query($db, $sqlQuery);
+	if($result->num_rows > 1){
+		echo "Error: Same modul UID twice ";
+		exit();	
+	}
+	$row = $result->fetch_assoc();
+	$modulUIDforSubject_new=$row["uid"];
+	//var_dump($modulUIDforSubject_new);
+	//$sql = "INSERT INTO tx_rere_domain_model_fach (pid, fachnr, fachname, pruefer, notenschema, modulnr, note, tstamp, crdate, cruser_id, deleted, hidden) VALUES
+
+}
+
 echo "done";
 ?>
