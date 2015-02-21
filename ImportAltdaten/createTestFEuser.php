5808a94ae4f8eac507d6f903d4105b35fd9095c4
ti-wa <tim@tims-MacBook-Air.local>
2015-02-21 17:24:33 +0100

    erzeugen von Prüflingen + Script um FE User zum Testen zu erzeugen

---
+++ ImportAltdaten/createTestFEuser.php
@@ -0,0 +1,31 @@
+<?php
+
+function crossfoot ( $digits )
+  {
+    // Typcast falls Integer uebergeben
+    $strDigits = ( string ) $digits;
+
+    for( $intCrossfoot = $i = 0; $i < strlen ( $strDigits ); $i++ )
+    {
+      $intCrossfoot += $strDigits{$i};
+    }
+
+    return $intCrossfoot;
+  } 
+
+
+$DB_URL="localhost";
+$DB_USERNAME="root";
+$DB_PASSWORD="";	
+$DB_NAME="typo3";
+$db = mysqli_connect($DB_URL, $DB_USERNAME, $DB_PASSWORD,$DB_NAME);
+
+for($i = 1; $i < 2000 ; $i++){
+	$j = crossfoot($i);
+	$sql = "INSERT INTO fe_users (first_name, last_name) VALUES (".$i.",".$j.")";
+	if (!mysqli_query($db, $sql)) {
+				echo "<br>Error: " . $sql . "<br>" . mysqli_error($db); 
+				exit();
+	}					
+}
+?>
